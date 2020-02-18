dbb6801bbc5bb1e13623e3f1a397fc24e4f2f622
AnonymousICT <kuohoward@gmail.com>
2020-02-17 16:28:41 -0800

    dummy data shows up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import Chatkit from '@pusher/chatkit'
+import Chatkit from '@pusher/chatkit-client'
+import MessageList from './components/messageList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import  { tokenUrl, instanceLocator } from './config'
+
+class App extends React.Component {
+
+  componentDidMount(){
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: instanceLocator,
+      userId: 'LemonICT',
+      tokenProvider: new Chatkit.TokenProvider({
+      url: tokenUrl
+    })
+    })
+
+    chatManager.connect()
+    .then(currentUser => {
+      currentUser.subscribeToRoomMultipart({
+        roomId: 'fe4ec7e3-5778-43f3-b164-7eac94aefb66',
+        hooks: {
+          onNewMessage: message => {
+            console.log(currentUser.id, message.parts[0].payload.content, message.createdAt)
+          }
+        }
+      });
+    })
+    .catch(error =>{
+      console.error("error", error);
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <MessageList />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/ec06ae86-120e-442c-8b67-12807de9e3cd/token";
+
+const instanceLocator = "v1:us1:ec06ae86-120e-442c-8b67-12807de9e3cd";
+
+export { tokenUrl, instanceLocator }
